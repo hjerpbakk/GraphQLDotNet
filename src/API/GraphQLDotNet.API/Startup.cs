12d86304f21ee2b4d45a222912d149e85b4cd90f
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2019-10-31 10:48:43 +0100

    Added GraphiQL for running test queries

--- src/API/GraphQLDotNet.API/Startup.cs
+++ src/API/GraphQLDotNet.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GraphiQl;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -37,6 +38,7 @@ namespace GraphQLDotNet.API
             }
 
             //app.UseHttpsRedirection();
+            app.UseGraphiQl();
 
             app.UseRouting();
 
