84c7b88fa36ceb8964eeec6e80d9eb6f7fcc6d64
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2019-11-26 08:03:36 +0100

    Removed TODO

--- src/Mobile/GraphQLDotNet.Mobile/OpenWeather/IOpenWeatherClient.cs
+++ src/Mobile/GraphQLDotNet.Mobile/OpenWeather/IOpenWeatherClient.cs
@@ -1,13 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using GraphQLDotNet.Contracts;
-using GraphQLDotNet.Mobile.Models;
 
 namespace GraphQLDotNet.Mobile.OpenWeather
 {
     public interface IOpenWeatherClient
     {
-        // TODO: Contracts vs. model objects
         Task<WeatherForecast> GetWeatherForecast(long locationId);
         Task<IEnumerable<WeatherLocation>> GetLocations(string searchTerm = "", int maxNumberOfResults = 8);
         Task<WeatherSummary> GetWeatherSummaryFor(long locationId);
