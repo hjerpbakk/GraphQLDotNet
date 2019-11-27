3a63bfed102cffcfe68064f86a191e987602d0b5
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2019-11-27 21:09:10 +0100

    Update WeatherForecastQuery.cs

--- src/API/GraphQLDotNet.API/GraphTypes/WeatherForecastQuery.cs
+++ src/API/GraphQLDotNet.API/GraphTypes/WeatherForecastQuery.cs
@@ -7,31 +7,30 @@ namespace GraphQLDotNet.API.GraphTypes
     {
         public WeatherForecastQuery(IOpenWeatherClient openWeatherClient)
         {
-            // TODO: Improve descriptions
-            Field<WeatherForecastType>("forecast", "Get forecast for the next dayz.",
+            Field<WeatherForecastType>("forecast", "Get the current forecast.",
                 new QueryArguments(
-                        new QueryArgument<NonNullGraphType<LongGraphType>> { Name = "city_id", Description = "Id of the city to fetch weather from." }
+                        new QueryArgument<NonNullGraphType<LongGraphType>> { Name = "city_id", Description = "Id of the location from which to fetch forecasts." }
                     ),
                 context =>
                 {
                     return openWeatherClient.GetWeatherFor(context.GetArgument<long>("city_id"));
                 });
-            Field<ListGraphType<WeatherSummaryType>>("summaries", "Get summaries for the given locations.",
+            Field<ListGraphType<WeatherSummaryType>>("summaries", "Get forecast summaries for the given locations.",
                 new QueryArguments(
-                        new QueryArgument<NonNullGraphType<ListGraphType<LongGraphType>>> { Name = "location_ids", Description = "Ids of the locations to fetch weather from." }
+                        new QueryArgument<NonNullGraphType<ListGraphType<LongGraphType>>> { Name = "location_ids", Description = "Ids of the locations from which to fetch forecasts." }
                     ),
                 context =>
                 {
                     return openWeatherClient.GetWeatherSummaryFor(context.GetArgument<long[]>("location_ids"));
                 });
-            Field<ListGraphType<WeatherLocationType>>("locations", "Available locations",
+            Field<ListGraphType<WeatherLocationType>>("locations", "Get locations with available forecasts.",
                 new QueryArguments(
-                        new QueryArgument<StringGraphType> { Name = "beginsWith", Description = "Characters the location begins with", DefaultValue = "" },
-                        new QueryArgument<IntGraphType> { Name = "maxResults", Description = "Max number of results", DefaultValue = IOpenWeatherClient.MaxNumberOfResults }
+                        new QueryArgument<StringGraphType> { Name = "searchTerms", Description = "Location and optional country after a ,.", DefaultValue = "" },
+                        new QueryArgument<IntGraphType> { Name = "maxResults", Description = "The max number of results to return.", DefaultValue = IOpenWeatherClient.MaxNumberOfResults }
                     ),
                 context =>
                 {
-                    return openWeatherClient.GetLocations(context.GetArgument<string>("beginsWith"), context.GetArgument<int>("maxResults"));
+                    return openWeatherClient.GetLocations(context.GetArgument<string>("searchTerms"), context.GetArgument<int>("maxResults"));
                 });
         }
     }
